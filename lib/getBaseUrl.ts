4c928f1b9822a32af3999f760569400ea3c7de23
dhanushvarma525 <dhanushvarmajampana@example.com>
2026-07-20 16:38:59 +0530

    Remove debug log from base URL

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -1,13 +1,9 @@
 export function getBaseUrl() {
 
-  console.log(
-    "NEXT_PUBLIC_SITE_URL =",
-    process.env.NEXT_PUBLIC_SITE_URL
-  );
-
   if (process.env.NEXT_PUBLIC_SITE_URL) {
     return process.env.NEXT_PUBLIC_SITE_URL;
   }
 
   return "http://localhost:3000";
+
 }
