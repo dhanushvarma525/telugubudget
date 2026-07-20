252e1199eae8d38a8d01b6eab4356e67ce301518
dhanushvarma525 <dhanushvarmajampana@example.com>
2026-07-20 16:26:36 +0530

    Debug base url

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -1,4 +1,10 @@
 export function getBaseUrl() {
+
+  console.log(
+    "NEXT_PUBLIC_SITE_URL =",
+    process.env.NEXT_PUBLIC_SITE_URL
+  );
+
   if (process.env.NEXT_PUBLIC_SITE_URL) {
     return process.env.NEXT_PUBLIC_SITE_URL;
   }
