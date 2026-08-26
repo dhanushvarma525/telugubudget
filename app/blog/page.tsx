0708867cbdd4990be7157ee9dfa7b4365d6afeb1
dhanushvarma525 <dhanushvarmajampana@example.com>
2026-08-26 12:00:18 +0530

    Update AnantaGo blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
+
 import { supabase } from "@/lib/supabase";
 
 type Blog = {
